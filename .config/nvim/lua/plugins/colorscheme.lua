a9d35d3216f092d71b638d771e3dddaca23e2208
Sai Kumar <ksai44576@gmail.com>
2024-07-02 11:52:47 +0530

    Add catppuccin colorscheme for Neovim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function ()
+        require("catppuccin").setup({
+            flavor = "mocha",
+        })
+
+        vim.cmd.colorscheme("catppuccin")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+}
