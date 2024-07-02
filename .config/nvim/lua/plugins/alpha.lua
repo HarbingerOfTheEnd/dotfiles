aa0e8c59f524402cdcf3ad309ef058da05a2ddd3
Sai Kumar <ksai44576@gmail.com>
2024-07-02 12:15:28 +0530

    feat: Add alpha-nvim plugin for customizing Neovim dashboard

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,31 @@
+return {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
+
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+            dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("q", " > Quit Neovim", "<cmd>qa<CR>"),
+        }
+
+        alpha.setup(dashboard.opts)
+
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
+}
