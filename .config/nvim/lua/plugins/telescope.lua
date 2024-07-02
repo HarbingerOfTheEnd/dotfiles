4251a0366caa6a62a5a07709521d387f17558e2b
Sai Kumar <ksai44576@gmail.com>
2024-07-02 12:24:38 +0530

    refactor: Use built-in functions for keymaps in telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
     },
     config = function()
         local telescope = require("telescope")
+        local builtin = require("telescope.builtin")
 
         telescope.setup({
             defaults = {
@@ -42,9 +43,9 @@ return {
 
         local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fr", telescope.oldfiles, { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fs", telescope.live_grep, { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fc", telescope.grep_string, { desc = "Find string under cursor in cwd" })
+        keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
     end,
 }
