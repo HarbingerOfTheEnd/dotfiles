94f15e233094d158aea1006268dc28181373e210
Sai Kumar <ksai44576@gmail.com>
2024-07-04 18:26:27 +0530

    Changed options to set transparent background

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function ()
-        require("catppuccin").setup({
-            flavor = "mocha",
-        })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavor = "mocha",
+			transparent_background = true,
+		})
 
-        vim.cmd.colorscheme("catppuccin")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
+		vim.cmd.colorscheme("catppuccin")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+	end,
 }
